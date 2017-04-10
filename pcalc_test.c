bb7f8890207754d68672656dcb585dfd9328c1c0
James Punnett <jppunnett@yahoo.com>
2017-04-10 05:58:58 -0400

    Rename barbell to bar

--- pcalc_test.c
+++ pcalc_test.c
@@ -15,13 +15,13 @@ plates_equal(float actual[], float expected[])
 
 int main()
 {
-	const float barbell_weight = 45;
+	const float bar_weight = 45;
 	// Decision: list of plates pre-sorted from highest to lowest and ends with
 	// zero sentinel. 
 	const float plates_avail[] = {45, 35, 25, 15, 10, 5, 2.5, 1.5, 0};
 
 	// Calculate plates for 100 lb load
-	float *actual = calc_plates(100, barbell_weight, plates_avail);
+	float *actual = calc_plates(100, bar_weight, plates_avail);
 	assert(actual);
 
 	float expected[] = {25, 2.5, 0};
