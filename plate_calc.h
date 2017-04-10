85e9fb9a44b50978f31589e9df0cb8272f56c46c
James Punnett <jppunnett@yahoo.com>
2017-04-10 05:58:23 -0400

    Rename barbell to bar

--- plate_calc.h
+++ plate_calc.h
@@ -1,7 +1,6 @@
 #ifndef PLATE_CALC_H
 #define PLATE_CALC_H
 
-float* calc_plates(float target_weight, float barbell_weight, 
-	const float plates_avail[]);
+float* calc_plates(float target_weight, float bar_weight, const float plates_avail[]);
 
 #endif
